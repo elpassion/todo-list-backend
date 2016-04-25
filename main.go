7eb9dc10631406dd038ffffde413de93e31819db
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2016-04-25 12:45:15 +0200

    Reorganize

--- main.go
+++ main.go
@@ -1,38 +1,11 @@
 package main
 
 import (
-    "fmt"
     "log"
     "net/http"
-
-    "github.com/gorilla/mux"
-    "encoding/json"
 )
 
 func main() {
-    router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", Index)
-    router.HandleFunc("/todos", TodoIndex)
-    router.HandleFunc("/todos/{todoId}", TodoShow)
-
+    router := NewRouter()
     log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Welcome!")
-}
-
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-    todos := Todos{
-        Todo{Name: "Write presentation"},
-        Todo{Name: "Host meetup"},
-    }
-
-    json.NewEncoder(w).Encode(todos)
-}
-
-func TodoShow(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    todoId := vars["todoId"]
-    fmt.Fprintln(w, "Todo show:", todoId)
 }
