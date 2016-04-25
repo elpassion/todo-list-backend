091d0dc816215e0dbda7a71d47bbf7fe108f9e98
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2016-04-25 12:50:18 +0200

    Apply logger

--- Routes.go
+++ Routes.go
@@ -16,14 +16,18 @@ type Route struct {
 type Routes []Route
 
 func NewRouter() *mux.Router {
-
     router := mux.NewRouter().StrictSlash(true)
     for _, route := range routes {
+        var handler http.Handler
+
+        handler = route.HandlerFunc
+        handler = Logger(handler, route.Name)
+
         router.
-        Methods(route.Method).
-        Path(route.Pattern).
-        Name(route.Name).
-        Handler(route.HandlerFunc)
+            Methods(route.Method).
+            Path(route.Pattern).
+            Name(route.Name).
+            Handler(handler)
     }
 
     return router
